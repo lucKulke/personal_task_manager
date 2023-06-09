8e6de9e94973b707c8eb0ce7fef0074da65fefc7
lucKulke <lucaskulke98@gmail.com>
2023-06-09 18:55:11 +0200

    added lists route with function to add more lists

--- todo.rb
+++ todo.rb
@@ -2,6 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do 
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do 
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+  erb "task manager", layout: :layout
+end
+
+get "/lists" do 
+  @lists = session[:lists]
+  # [
+  #   {name: "first list", todos: []},
+  #   {name: "second list", todos: []}
+  # ]
+
+  erb :lists
+end
+
+get "/lists/new" do 
+  session[:lists] << {name: "new list", todos: []}
+  redirect "/lists"
 end
