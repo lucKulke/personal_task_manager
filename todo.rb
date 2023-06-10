a98948d73ae4f841bfe1a1ae012b78551975b8f3
lucKulke <lucaskulke98@gmail.com>
2023-06-10 13:49:27 +0200

    back to begining

--- todo.rb
+++ todo.rb
@@ -2,31 +2,6 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-configure do 
-  enable :sessions
-  set :session_secret, 'secret'
-end
-
-before do 
-  session[:lists] ||= []
-end
-
 get "/" do
-  redirect "/lists"
-  erb "task manager", layout: :layout
-end
-
-get "/lists" do 
-  @lists = session[:lists]
-  # [
-  #   {name: "first list", todos: []},
-  #   {name: "second list", todos: []}
-  # ]
-
-  erb :lists
-end
-
-get "/lists/new" do 
-  session[:lists] << {name: "new list", todos: []}
-  redirect "/lists"
+  erb "You have no lists.", layout: :layout
 end
